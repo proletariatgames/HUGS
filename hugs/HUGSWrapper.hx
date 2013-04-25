523fe978798a6968a140bc4766888ec88e74d57f
Dan Ogles <danogles@gmail.com>
2013-04-25 14:18:19 -0400

    Added addTypedCompnent to HugsWrapper

--- hugs/HUGSWrapper.hx
+++ hugs/HUGSWrapper.hx
@@ -8,4 +8,8 @@ class HUGSWrapper {
   public static function getTypedComponent<T>(c:Component, type:Class<T>):T {
     return cast c.GetComponent(Type.getClassName(type));
   }
+
+  public static function addTypedComponent<T>(g:GameObject, type:Class<T>):T {
+    return cast g.AddComponent(Type.getClassName(type));
+  }
 }
