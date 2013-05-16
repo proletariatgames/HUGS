3124474132695644dc10e24332e61e77383ddbe5
Dan Ogles <danogles@gmail.com>
2013-05-16 17:22:21 -0400

    Fix compile error in GUILayout, added Vector3/Quaternion methods in HUGSWrapper

--- hugs/HUGSWrapper.hx
+++ hugs/HUGSWrapper.hx
@@ -1,7 +1,9 @@
 package hugs;
 
-import unityengine.Component;
 import unityengine.GameObject;
+import unityengine.Component;
+import unityengine.Quaternion;
+import unityengine.Vector3;
 
 import dotnet.system.collections.IEnumerable;
 import dotnet.system.collections.IEnumerator;
@@ -22,6 +24,36 @@ class HUGSWrapper {
 
   public static function adaptEnumerableT<T>(enumerable:IEnumerable, type:Class<T>) : EnumeratorAdapter<T> {
     return new EnumeratorAdapter<T>(enumerable.GetEnumerator());
+  }
+}
+
+class QuaternionMethods
+{
+  public static inline function mul(a:Quaternion, b:Quaternion) : Quaternion {
+    return untyped __cs__("a*b");
+  }
+
+  public static inline function rotatePoint(a:Quaternion, b:Vector3) : Vector3 {
+    return untyped __cs__("a*b");
+  }
+}
+
+class Vector3Methods
+{
+  public static inline function add(a:Vector3, b:Vector3) : Vector3 {
+    return untyped __cs__("a+b");
+  }
+
+  public static inline function sub(a:Vector3, b:Vector3) : Vector3 {
+    return untyped __cs__("a-b");
+  }
+
+  public static inline function mul(a:Vector3, b:Float) : Vector3 {
+    return untyped __cs__("a*b");
+  }
+
+  public static inline function div(a:Vector3, b:Float) : Vector3 {
+    return untyped __cs__("a/b");
   }
 }
 
