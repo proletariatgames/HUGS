66415102a4593a91ce39beabf4e08f4ea128db6f
Dan Ogles <danogles@gmail.com>
2013-07-15 16:43:52 -0400

    Removed Reflection.Emit API for now. There is a collision between the Haxe builtin cs.system.Type and the dotnet.system.Type.

--- dotnet/system/reflection/emit/PEFileKinds.hx
+++
@@ -1,9 +0,0 @@
-package dotnet.system.reflection.emit;
-
-@:fakeEnum(Int) @:native("System.Reflection.Emit.PEFileKinds")
-extern enum PEFileKinds {
-  Dll;
-  ConsoleApplication;
-  WindowApplication;
-}
-
