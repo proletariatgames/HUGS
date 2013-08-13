e24c899842f4a5cc16167e75024bad8f10ab4c0c
AxGord <axgord@gmail.com>
2013-08-13 17:09:41 +0600

    More inline

--- hugs/HUGSWrapper.hx
+++ hugs/HUGSWrapper.hx
@@ -3,6 +3,7 @@ package hugs;
 import unityengine.GameObject;
 import unityengine.Component;
 import unityengine.Quaternion;
+import unityengine.Transform;
 import unityengine.Vector3;
 
 import dotnet.system.collections.IEnumerable;
@@ -10,87 +11,104 @@ import dotnet.system.collections.IEnumerator;
 
 class HUGSWrapper {
 
-  public static function getTypedComponent<T>(c:Component, type:Class<T>):T {
+  inline public static function getTypedComponent<T>(c:Component, type:Class<T>):T {
     return cast c.GetComponent(cs.Lib.toNativeType(type).Name);
   }
 
-  public static function adaptEnumerable(enumerable:IEnumerable) : Iterator<Dynamic> {
+  inline public static function adaptEnumerable(enumerable:IEnumerable) : Iterator<Dynamic> {
     return new EnumeratorAdapter<Dynamic>(enumerable.GetEnumerator());
   }
 
-  public static function adaptEnumerableT<T>(enumerable:IEnumerable, type:Class<T>) : Iterator<T> {
+  inline public static function adaptEnumerableT<T>(enumerable:IEnumerable, type:Class<T>) : Iterator<T> {
     return new EnumeratorAdapter<T>(enumerable.GetEnumerator());
   }
 
-  public static function getComponentInChildrenOfType<T>(c:Component, type:Class<T>) : T { 
+  inline public static function getComponentInChildrenOfType<T>(c:Component, type:Class<T>) : T { 
     return cast c.GetComponentInChildren(cs.Lib.toNativeType(type));
   }
 
-  public static function getComponentsInChildrenOfType<T>(c:Component, type:Class<T>) : Iterator<T> {
+  inline public static function getComponentsInChildrenOfType<T>(c:Component, type:Class<T>) : NativeArrayIterator<T> {
     var t:cs.system.Type = cs.Lib.toNativeType(type);
     return cast new NativeArrayIterator<Component>(c.GetComponentsInChildren(t));
   }
+  
+  inline public static function getOrAddTypedComponent<T>(c:Component, type:Class<T>):T
+    return GameObjectMethods.getOrAddTypedComponent(c.gameObject, type);
+		
+  inline public static function getChildGameObject(c:Component, name:String):GameObject
+    return GameObjectMethods.getChildGameObject(c.gameObject, name);
 }
 
 class GameObjectMethods
 {
-  public static function getTypedComponent<T>(g:GameObject, type:Class<T>):T {
+  inline public static function getTypedComponent<T>(g:GameObject, type:Class<T>):T {
     return cast g.GetComponent(cs.Lib.toNativeType(type).Name);
   }
 
-  public static function addTypedComponent<T>(g:GameObject, type:Class<T>):T {
+  inline public static function addTypedComponent<T>(g:GameObject, type:Class<T>):T {
     return cast g.AddComponent(cs.Lib.toNativeType(type).Name);
   }
 
-  public static function getComponentsOfType<T>(g:GameObject, type:Class<T>) : Iterator<T> {
+  public static function getComponentsOfType<T>(g:GameObject, type:Class<T>) : NativeArrayIterator<T> {
     var t:cs.system.Type = cs.Lib.toNativeType(type);
     return cast new NativeArrayIterator<Component>(g.GetComponents(t));
   }
 
-  public static function getComponentsInChildrenOfType<T>(g:GameObject, type:Class<T>) : Iterator<T> {
+  public static function getComponentsInChildrenOfType<T>(g:GameObject, type:Class<T>) : NativeArrayIterator<T> {
     var t:cs.system.Type = cs.Lib.toNativeType(type);
     return cast new NativeArrayIterator<Component>(g.GetComponentsInChildren(t));
   }
+  
+  public static function getOrAddTypedComponent<T>(c:GameObject, type:Class<T>):T {
+    var o:T = getTypedComponent(c, type);
+    return o == null ? GameObjectMethods.addTypedComponent(c.gameObject, type) : o;
+  }
+
+  public static function getChildGameObject(gameObject:GameObject, name:String):GameObject {
+    for (t in getComponentsInChildrenOfType(gameObject, Transform)) if (t.gameObject.name == name) return t.gameObject;
+    return null;
+  }
+  
 }
 
 class QuaternionMethods
 {
-  public static function mulVector3(a:Quaternion, b:Vector3) : Vector3 {
+  inline public static function mulVector3(a:Quaternion, b:Vector3) : Vector3 {
     return untyped __cs__("a*b");
   }
 
-  public static function mul(a:Quaternion, b:Quaternion) : Quaternion {
+  inline public static function mul(a:Quaternion, b:Quaternion) : Quaternion {
     return untyped __cs__("a*b");
   }
 
-  public static function rotatePoint(a:Quaternion, b:Vector3) : Vector3 {
+  inline public static function rotatePoint(a:Quaternion, b:Vector3) : Vector3 {
     return untyped __cs__("a*b");
   }
 
-  public static function eq(a:Quaternion, b:Quaternion) : Bool {
+  inline public static function eq(a:Quaternion, b:Quaternion) : Bool {
     return untyped __cs__("a==b");
   }
 }
 
 class Vector3Methods
 {
-  public static function add(a:Vector3, b:Vector3) : Vector3 {
+  inline public static function add(a:Vector3, b:Vector3) : Vector3 {
     return untyped __cs__("a+b");
   }
 
-  public static function sub(a:Vector3, b:Vector3) : Vector3 {
+  inline public static function sub(a:Vector3, b:Vector3) : Vector3 {
     return untyped __cs__("a-b");
   }
 
-  public static function mul(a:Vector3, b:Single) : Vector3 {
+  inline public static function mul(a:Vector3, b:Single) : Vector3 {
     return untyped __cs__("b*a");
   }
 
-  public static function div(a:Vector3, b:Single) : Vector3 {
+  inline public static function div(a:Vector3, b:Single) : Vector3 {
     return untyped __cs__("a/b");
   }
 
-  public static function eq(a:Vector3, b:Vector3) : Bool {
+  inline public static function eq(a:Vector3, b:Vector3) : Bool {
     return untyped __cs__("a==b");
   }
 }
@@ -99,7 +117,7 @@ class Vector3Methods
 class EnumeratorAdapter<T>
 {
   public var enumerator(default, null):IEnumerator;
-  public function new(enumerator:IEnumerator) {
+  inline public function new(enumerator:IEnumerator) {
     this.enumerator = enumerator;
   }
 
@@ -118,16 +136,23 @@ class NativeArrayIterator<T>
 {
   public var array(default, null):cs.NativeArray<T>;
   public var i:Int;
-  public function new(ar:cs.NativeArray<T>) {
+  inline public function new(ar:cs.NativeArray<T>) {
     this.array = ar;
     this.i = 0;
   }
 
-  public function next() : T {
-    return this.array[i++];
-  }
+  public function next() : T return this.array[i++];
 
-  public function hasNext() : Bool {
-    return i < this.array.Length;
+  public function hasNext() : Bool return i < this.array.Length;
+  
+  inline public function reset():Void i = 0;
+  
+  inline public function keys():IntIterator return 0...array.Length;
+  
+  public function haxeArray():Array<T> {
+	var a:Array<T> = [];
+	for (e in this) a.push(e);
+	return a;
   }
+  
 }
