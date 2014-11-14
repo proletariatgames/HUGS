de6baeec9c4482c4b65df87503be9604fc817d2f
Luke Petre <luke@proletariat.com>
2014-11-14 18:36:05 -0500

    Removing deprecated LightmapData.lightmap

--- unityengine/LightmapData.hx
+++ unityengine/LightmapData.hx
@@ -3,7 +3,6 @@ package unityengine;
 @:native("UnityEngine.LightmapData") @:final
 extern class LightmapData extends dotnet.system.Object {
   public var lightmapFar : Texture2D;
-  public var lightmap : Texture2D;
   public var lightmapNear : Texture2D;
 
   public function new() : Void;
